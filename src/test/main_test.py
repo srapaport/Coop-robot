fd6d7ab76b129a74a10fbde3243bb6515c29dca2
srapaport <solal.rapaport@gmail.com>
2022-06-16 14:19:07 +0200

    nouvelle arborescence

--- src/test/main_test.py
+++ src/test/main_test.py
@@ -1,10 +1,11 @@
 from z3 import *
 from utilZ3v5 import *
 
-taille_anneau = 4       # Taille de l'anneau 
-nb_robots = 3           # Nombre de robot sur l'anneau
+taille_anneau = 3       # Taille de l'anneau 
+nb_robots = 2           # Nombre de robot sur l'anneau
 
 taille_boucle_max = (factorial(8 * taille_anneau + nb_robots -1)) // (factorial(nb_robots) * factorial(8 * taille_anneau - 1))
+print("borne supérieur de la taille du graphe : ", taille_boucle_max, "\n")
 
 p = [ Int('p%s' % i) for i in range(nb_robots) ]
 s = [ Int('s%s' % i) for i in range(nb_robots) ]
@@ -30,7 +31,7 @@ t_prime3 = [ Int('tp3%s' % i) for i in range(nb_robots) ]
 solv1 = Solver()
 
 tabInit = InitSM(p, s, t, taille_anneau)
-# solv1.add(tabInit)
+solv1.add(tabInit)
 # tabConfig1 = ConfigView(taille_anneau, nb_robots, 0, p, d0)
 # solv1.add(tabConfig1)
 # tabViewSym1 = ViewSym(taille_anneau, nb_robots, d0, d0_prime)
@@ -59,23 +60,23 @@ tabInit = InitSM(p, s, t, taille_anneau)
 # tabBP1 = BouclePerdante_v2(taille_anneau, p, s, t, 4, phiSM)
 # solv1.add(tabBP1)
 
-for i in range(1, taille_boucle_max):
-        solv1 = Solver()
-        print("Main appelle BouclePerdante de taille : ", i, "\n")
-        tabBP1 = BouclePerdante(taille_anneau, p, s, t, i, phiSM)
-        solv1.add(tabInit)
-        solv1.add(tabBP1)
-        c = solv1.check()
-        print("solv1 : ", c)
-        if(c == sat):
-                print("model :\n",solv1.model().sexpr())
-                break;
+# for i in range(1, taille_boucle_max):
+#         solv1 = Solver()
+#         print("Main appelle BouclePerdante de taille : ", i, "\n")
+#         tabBP1 = BouclePerdante(taille_anneau, p, s, t, i, phiSM)
+#         solv1.add(tabInit)
+#         solv1.add(tabBP1)
+#         c = solv1.check()
+#         print("solv1 : ", c, "\n")
+#         if(c == sat):
+#                 print("model :\n",solv1.model().sexpr())
+#                 break;
 
 
-# c = solv1.check()
-# print("solv1 : ", c)
-# if(c == sat):
-#         print("model :\n",solv1.model())
+c = solv1.check()
+print("solv1 : ", c)
+if(c == sat):
+        print("model :\n",solv1.model().sexpr())
 
 # tabPhiSM2 = phiSM(d1)
 # tabConfig2 = ConfigView(taille_anneau, nb_robots, 1, p, d1)
