b2a9af7141fed65baa3a959f90c7b33f662e9b41
srapaport <solal.rapaport@gmail.com>
2022-07-12 19:41:52 +0200

    compar phiR

--- data/data.py
+++ data/data.py
@@ -4,28 +4,54 @@ from mpl_toolkits.mplot3d import axes3d
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('./data/data-phiSimple/data-timev2.csv', sep=';')
-
-algov5 = data.loc[(data['algo'] == 5) & (data['error'] == 0)]
-algov7 = data.loc[(data['algo'] == 7) & (data['error'] == 0)]
-algov5err = data.loc[(data['algo'] == 5) & (data['error'] == 1)]
-algov7err = data.loc[(data['algo'] == 7) & (data['error'] == 1)]
-
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-ax = plt.subplot(111, projection='3d')
-
-ax.scatter(algov5['taille anneau'], algov5['nb robots'], algov5['time elapsed'], label='algo5', marker='d', color='yellow', linewidths=20)
-ax.scatter(algov5err['taille anneau'], algov5err['nb robots'], algov5err['time elapsed'], label='algo5err', marker='d', color='orange', linewidths=20)
-ax.scatter(algov7['taille anneau'], algov7['nb robots'], algov7['time elapsed'], label='algo7', marker='d', color='green', linewidths=10)
-ax.scatter(algov7err['taille anneau'], algov7err['nb robots'], algov7err['time elapsed'], label='algo7err', marker='d', color='purple', linewidths=10)
+data = pd.read_csv('./data/data-phiR/data-timev2.csv', sep=';')
 
 
-plt.title("Elapsed time depending on ring size and robot number", fontsize=20)
-ax.set_xlabel('Taille anneau', fontsize=20)
-ax.set_ylabel('Nb Robots', fontsize=20)
-ax.set_zlabel('Time', fontsize=20)
-ax.legend(fontsize=20)
-plt.tight_layout()
-# plt.savefig('./data/graphe-time')
-plt.show()
+algov5 = data.loc[(data['algo'] == 5) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov7 = data.loc[(data['algo'] == 7) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+
+algov5r2 = data.loc[(data['algo'] == 5) & (data['nb robots'] == 2) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov5r3 = data.loc[(data['algo'] == 5) & (data['nb robots'] == 3) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov5r4 = data.loc[(data['algo'] == 5) & (data['nb robots'] == 4) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov5r5 = data.loc[(data['algo'] == 5) & (data['nb robots'] == 5) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+
+algov7r2 = data.loc[(data['algo'] == 7) & (data['nb robots'] == 2) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov7r3 = data.loc[(data['algo'] == 7) & (data['nb robots'] == 3) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov7r4 = data.loc[(data['algo'] == 7) & (data['nb robots'] == 4) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+algov7r5 = data.loc[(data['algo'] == 7) & (data['nb robots'] == 5) & ((data['error'] == 0) | (data['error'] == 3) | (data['error'] == 2))]
+
+######################## 2D
+
+# fig, ax = plt.subplots()
+# ax.plot(algov5r2.sort_values(by = 'taille anneau')['taille anneau'], algov5r2.sort_values(by = 'taille anneau')['time elapsed'], color='blue', label='algov5r2')
+# ax.plot(algov7r2.sort_values(by = 'taille anneau')['taille anneau'], algov7r2.sort_values(by = 'taille anneau')['time elapsed'], color='red', label='algov7r2')
+
+# ax.set(xlabel='taille anneau', ylabel='time elapsed', title='Time elapsed per size of the ring for 2 robots')
+# ax.grid()
+# ax.legend()
+# #fig.savefig("./data/compar2.png")
+# plt.show()
+
+######################## 3D
+
+# algov5err = data.loc[(data['algo'] == 5) & ((data['error'] == 0) | (data['error'] == 3))]
+# algov7err = data.loc[(data['algo'] == 7) & ((data['error'] == 0) | (data['error'] == 3))]
+
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+# ax = plt.subplot(111, projection='3d')
+
+# ax.scatter(algov5['taille anneau'], algov5['nb robots'], algov5['time elapsed'], label='algo5', marker='d', color='yellow', linewidths=20)
+# #ax.scatter(algov5err['taille anneau'], algov5err['nb robots'], algov5err['time elapsed'], label='algo5err', marker='d', color='orange', linewidths=20)
+# ax.scatter(algov7['taille anneau'], algov7['nb robots'], algov7['time elapsed'], label='algo7', marker='d', color='green', linewidths=10)
+# #ax.scatter(algov7err['taille anneau'], algov7err['nb robots'], algov7err['time elapsed'], label='algo7err', marker='d', color='purple', linewidths=10)
+
+
+# plt.title("Elapsed time depending on ring size and robot number", fontsize=20)
+# ax.set_xlabel('Taille anneau', fontsize=20)
+# ax.set_ylabel('Nb Robots', fontsize=20)
+# ax.set_zlabel('Time', fontsize=20)
+# ax.legend(fontsize=20)
+# plt.tight_layout()
+# # plt.savefig('./data/graphe-time')
+# plt.show()
