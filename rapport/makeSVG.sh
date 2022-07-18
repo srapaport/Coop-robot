6f97829c4fcfbbb714b4c4a967bf6837d36ba5bf
srapaport <solal.rapaport@gmail.com>
2022-07-18 12:34:37 +0200

    rename

---
+++ rapport/makeSVG.sh
@@ -0,0 +1,8 @@
+#§ /bin/bash
+for file in `ls`
+do
+    if [ `echo $file | grep -c -E '^.*\.svg$'` == 1 ]
+    then
+        inkscape -D -z --file=$file --export-pdf=`echo $file | sed -r -n 's/^(.*)\.svg$/\1/p'`.pdf --export-latex
+    fi
+done
