c5424801118c87c590d497d3f729162d0252a9e4
srapaport <solal.rapaport@gmail.com>
2022-07-29 12:45:27 +0200

    debug min 0x

--- scripts/conversion-temps.sh
+++ scripts/conversion-temps.sh
@@ -10,14 +10,16 @@ do
             hour=0
             min=`echo $data | sed -r -n 's/^.*;(.*):(.*)\.(.*)$/\1/p'`
             sec=`echo $data | sed -r -n 's/^.*;(.*):0?(.+)\.(.*)$/\2/p'`
-            milli=`echo $data | sed -r -n 's/^.*;(.*):(.*)\.(.*)$/\3/p'`
+            milli=`echo $data | sed -r -n 's/^.*;(.*):(.*)\.0?([1-9]?[0-9]{1})$/\3/p'`
         else
             hour=`echo $data | sed -r -n 's/^.*;(.*):(.*):(.*)$/\1/p'`
             min=`echo $data | sed -r -n 's/^.*;(.*):0?(.+):(.*)$/\2/p'`
-            sec=`echo $data | sed -r -n 's/^.*;(.*):(.*):(.*)$/\3/p'`
+            sec=`echo $data | sed -r -n 's/^.*;(.*):(.*):0?([1-9]?[0-9]{1})$/\3/p'`
             milli=0
         fi
         timeRes=$(((60*60*$hour) + (60*$min) + $sec))
+        echo -n $data
+        echo " $hour h: $min min: $sec s. $milli"
         echo $timeRes.$milli >> ../data/data-$1/data-timev2.csv
     fi
 done
