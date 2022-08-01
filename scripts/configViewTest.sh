db61a31f74670cdb5ce80eb6a73947bb8490ddfa
srapaport <solal.rapaport@gmail.com>
2022-08-01 12:12:33 +0200

    nettoyage + test gitignore

--- scripts/configViewTest.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-python3 ../src/utilZ3v5.py | grep d
--- scripts/makeSVG.sh
+++
@@ -1,8 +0,0 @@
-#§ /bin/bash
-for file in `ls`
-do
-    if [ `echo $file | grep -c -E '^.*\.svg$'` == 1 ]
-    then
-        inkscape -D -z --file=$file --export-pdf=`echo $file | sed -r -n 's/^(.*)\.svg$/\1/p'`.pdf --export-latex
-    fi
-done
