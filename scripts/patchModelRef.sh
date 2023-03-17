ad6479fafe1fe90a00ccab2b708d1fe5d93fb3f1
srapaport <solal.rapaport@gmail.com>
2023-03-17 17:32:47 +0900

    first commit for visibility visibility_improvement

--- scripts/patchModelRef.sh
+++ scripts/patchModelRef.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
-sudo patch $1 -d /usr/lib/python3.8/site-packages/z3/ < /usr/lib/python3.8/site-packages/z3/ajoutException.patch
+if [ $# == 0 ]
+then
+	echo "usage : ./patchModelRef.sh <python version> <reverse>"
+	exit 0
+fi
+if [ $# > 1 ]
+then
+	sudo patch --reverse -d /usr/lib/python$1/site-packages/z3/ < /usr/lib/python$1/site-packages/z3/ajoutException.patch
+	exit 0
+fi
+sudo patch -d /usr/lib/python$1/site-packages/z3/ < /usr/lib/python$1/site-packages/z3/ajoutException.patch
+exit 0
